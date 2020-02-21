ebcda70431821f8cbf7cf6450295eb27b7df5a36
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-02-21 21:19:07 +0100

    AddGroupServlet was added

---
+++ src/main/java/pl/coderslab/controller/AddGroupServlet.java
@@ -0,0 +1,27 @@
+package pl.coderslab.controller;
+
+import pl.coderslab.dao.GroupDao;
+import pl.coderslab.model.Group;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(value = "/admin/groups/add")
+public class AddGroupServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = request.getParameter("name");
+        GroupDao groupDao = new GroupDao();
+        Group group = new Group(name);
+        groupDao.create(group);
+        response.sendRedirect("/admin/groups");
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/WEB-INF/group-add.jsp").forward(request, response);
+
+    }
+}
