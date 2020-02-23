74290181f394c103eecb31b50bf40f277a0faf8d
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-02-23 19:11:08 +0100

    Url pattern was corrected

--- src/main/java/pl/coderslab/controller/IndexServlet.java
+++ src/main/java/pl/coderslab/controller/IndexServlet.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(value = "/")
+@WebServlet(value = "")
 public class IndexServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
