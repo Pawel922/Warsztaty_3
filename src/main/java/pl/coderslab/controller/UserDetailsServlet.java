01c8fcf07c90ec7611688c030330a4a280039d5b
Pawel Wozniak <pawel.jan.wozniak@gmail.com>
2020-02-29 21:18:24 +0100

    Additional code to show titles of exercises was added

--- src/main/java/pl/coderslab/controller/UserDetailsServlet.java
+++ src/main/java/pl/coderslab/controller/UserDetailsServlet.java
@@ -1,7 +1,9 @@
 package pl.coderslab.controller;
 
+import pl.coderslab.dao.ExerciseDao;
 import pl.coderslab.dao.SolutionDao;
 import pl.coderslab.dao.UserDao;
+import pl.coderslab.model.Exercise;
 import pl.coderslab.model.Solution;
 import pl.coderslab.model.User;
 
@@ -11,6 +13,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet(value = "/user/details")
 public class UserDetailsServlet extends HttpServlet {
@@ -19,8 +23,15 @@ public class UserDetailsServlet extends HttpServlet {
         int userId = Integer.parseInt(request.getParameter("id"));
         UserDao userDao = new UserDao();
         SolutionDao solutionDao = new SolutionDao();
+        ExerciseDao exerciseDao = new ExerciseDao();
         User user = userDao.read(userId);
         Solution[] solutions = solutionDao.findAllByUserId(userId);
+        List<Exercise> exercises = new ArrayList<>();
+        for(Solution solution : solutions){
+            int exerciseId = solution.getExerciseId();
+            Exercise exercise = exerciseDao.read(exerciseId);
+            solution.setExerciseTitle(exercise.getTitle());
+        }
         request.setAttribute("user", user);
         request.setAttribute("solutions", solutions);
         getServletContext().getRequestDispatcher("/WEB-INF/user-details.jsp").forward(request, response);
